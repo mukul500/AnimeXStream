1b473b13e796083a1e891718cc8b3ec6e2464c23
justdvnsh <justdvnsh2208@gmail.com>
2020-07-02 23:54:26 +0530

    minor fix for toggle mode

--- app/src/main/java/net/xblacky/animexstream/ui/main/home/HomeFragment.kt
+++ app/src/main/java/net/xblacky/animexstream/ui/main/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package net.xblacky.animexstream.ui.main.home
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.res.Configuration
 import android.net.Uri
@@ -18,12 +17,13 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.fragment_home.view.darkMode
 import net.xblacky.animexstream.BuildConfig
 import net.xblacky.animexstream.MainActivity
 import net.xblacky.animexstream.R
 import net.xblacky.animexstream.ui.main.home.epoxy.HomeController
-import net.xblacky.animexstream.utils.ThemeHelper
 import net.xblacky.animexstream.utils.constants.C
 import net.xblacky.animexstream.utils.model.AnimeMetaModel
 import timber.log.Timber
@@ -79,7 +79,6 @@ class HomeFragment : Fragment(), View.OnClickListener, HomeController.EpoxyAdapt
         rootView.search.setOnClickListener(this)
         rootView.favorite.setOnClickListener(this)
         rootView.darkMode.setOnClickListener(this)
-        rootView.lightMode.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -103,23 +102,15 @@ class HomeFragment : Fragment(), View.OnClickListener, HomeController.EpoxyAdapt
             R.id.darkMode -> {
                 toggleModes()
             }
-
-            R.id.lightMode -> {
-                toggleModes()
-            }
         }
     }
 
     private fun toggleModes() {
         if ((activity as MainActivity).resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            rootView.darkMode.visibility = GONE
-            rootView.lightMode.visibility = VISIBLE
             Toast.makeText(context, "Light Mode", Toast.LENGTH_SHORT).show()
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            rootView.lightMode.visibility = GONE
-            rootView.darkMode.visibility = VISIBLE
             Toast.makeText(context, "Night Mode", Toast.LENGTH_SHORT).show()
         }
     }
